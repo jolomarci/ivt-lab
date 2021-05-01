31a374c827636f696470999e17e4c38993c822ed
Marcell Balogh <bamarci10@gmail.com>
2021-05-01 18:56:00 +0200

    fixed Random issue again

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -12,6 +12,7 @@ public class TorpedoStore {
 
   // rate of failing to fire torpedos [0.0, 1.0]
   private double FAILURE_RATE = 0.0; //NOSONAR
+  Random generator = new Random();
 
   private int torpedoCount = 0;
 
@@ -37,7 +38,7 @@ public class TorpedoStore {
     boolean success = false;
 
     // simulate random overheating of the launcher bay which prevents firing
-    Random generator = SecureRandom.getInstanceStrong();
+    
     double r = generator.nextDouble();
 
     if (r >= FAILURE_RATE) {
